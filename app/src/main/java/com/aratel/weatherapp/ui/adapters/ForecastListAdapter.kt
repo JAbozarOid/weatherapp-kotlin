621ff2091d862b581f24294dff07e1efb067d131
JAbozarOid <araghibdoust@hotmail.com>
2020-05-16 18:23:59 +0430

    added item_forecast,edit on MainActivity and It's adapter-99.02.27

--- app/src/main/java/com/aratel/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/aratel/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -1,22 +1,43 @@
 package com.aratel.weatherapp.ui.adapters
 
 import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.aratel.weatherapp.R
+import com.aratel.weatherapp.domain.model.Forecast
 import com.aratel.weatherapp.domain.model.ForecastList
+import com.aratel.weatherapp.ui.utils.ctx
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.*
+import org.jetbrains.anko.find
+import java.util.zip.Inflater
 
-class ForecastListAdapter(private val weekForecast: ForecastList) :
+class ForecastListAdapter(
+    private val weekForecast: ForecastList,
+    private val itemClick: ForecastListAdapter.OnItemClickListener
+) :
     RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
 
-    override fun onCreateViewHolder(
+    // when the item layout is a simple textview
+    /*override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
         return ViewHolder(
             TextView(parent.context)
         )
+    }*/
+
+    // define item_forecast
+    // parent.ctx uses from file ui/utils/ViewExtension.kt >>> it’s another good example of how to use extension properties
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.ctx).inflate(R.layout.item_forecast, parent, false)
+        return ViewHolder(view, itemClick)
     }
 
     /**
@@ -36,9 +57,13 @@ class ForecastListAdapter(private val weekForecast: ForecastList) :
         /**
          * or we can get the size of the weekForecast list by implementation get method ForecastList data class in DomainClasses
          */
-        with(weekForecast[position]) {
+        // when the view is a single line of TextView
+        /*with(weekForecast[position]) {
             holder.textView.text = "$date - $description - $high/$low"
-        }
+        }*/
+
+        // when the view for items in list is item_forecast
+        holder.bindForecast(weekForecast[position])
     }
 
     override fun getItemCount(): Int {
@@ -49,7 +74,33 @@ class ForecastListAdapter(private val weekForecast: ForecastList) :
         return weekForecast.size
     }
 
+    // use this single line code when the view is a simple textview
+    //class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    // handle item_forecast view with this class
+    class ViewHolder(view: View, val itemClick: OnItemClickListener) :
+        RecyclerView.ViewHolder(view) {
+        private val iconView = view.find<ImageView>(R.id.icon)
+        private val dateView = view.find<TextView>(R.id.date)
+        private val descriptionView = view.find<TextView>(R.id.description)
+        private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
+        private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
+
+        fun bindForecast(forecast: Forecast) {
+            with(forecast) {
+                Picasso.get().load(iconUrl).into(iconView)
+                dateView.text = date
+                descriptionView.text = description
+                maxTemperatureView.text = "${high}º"
+                minTemperatureView.text = "${low}º"
+                itemView.setOnClickListener { itemClick(this) }
+            }
+        }
+    }
+
+    // the invoke method can be omitted when called
+    interface OnItemClickListener {
+        operator fun invoke(forecast: Forecast)
+    }
 
 }
