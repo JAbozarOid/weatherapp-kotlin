a1b777b5e005272907dce2d67dd3c2baf1019e7c
JAbozarOid <araghibdoust@hotmail.com>
2020-05-27 13:00:58 +0430

    RequestForecastCommand-99.03.07

--- app/src/main/java/com/aratel/weatherapp/domain/commands/RequestForecastCommand.kt
+++ app/src/main/java/com/aratel/weatherapp/domain/commands/RequestForecastCommand.kt
@@ -4,6 +4,8 @@ import com.aratel.weatherapp.data.ForecastRequest
 import com.aratel.weatherapp.domain.mappers.ForecastDataMapper
 import com.aratel.weatherapp.domain.model.ForecastList
 
+// val means the zipCode value can only be requested, but not modified which means immutable
+// private means shouldn't be visible
 class RequestForecastCommand(private val zipCode: String) : Command<ForecastList> {
 
     override fun execute(): ForecastList {
