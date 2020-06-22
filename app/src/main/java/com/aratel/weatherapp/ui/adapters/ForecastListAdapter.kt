6accf8875a072d0e799d97db18830a5658140a61
JAbozarOid <araghibdoust@hotmail.com>
2020-06-22 19:31:07 +0430

    using kotlin extension import instead of findviewbyid-99.04.02

--- app/src/main/java/com/aratel/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/aratel/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -87,19 +87,21 @@ class ForecastListAdapter(
         val itemClick: (Forecast) -> Unit
     ) :
         RecyclerView.ViewHolder(view) {
-        private val iconView = view.find<ImageView>(R.id.icon)
-        private val dateView = view.find<TextView>(R.id.date)
-        private val descriptionView = view.find<TextView>(R.id.description)
-        private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
-        private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
+
+        //private val iconView = view.find<ImageView>(R.id.icon)
+        //private val dateView = view.find<TextView>(R.id.date)
+        //private val descriptionView = view.find<TextView>(R.id.description)
+        //private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
+        //private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
 
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.get().load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high}º"
-                minTemperatureView.text = "${low}º"
+                // *** when using import kotlinx.android.synthetic.main.item_forecast.view.* NOT using find
+                Picasso.get().load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high}º"
+                itemView.minTemperature.text = "${low}º"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
